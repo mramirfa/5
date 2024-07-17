40bedb56ce57bd10dedfd45e0dc3145c3996078e
aMIr <166694766+mramirfa@users.noreply.github.com>
2024-07-17 02:31:02 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+  speed: 500,
+  slidesPerView: "auto",
+  spaceBetween: 15,
+
+  // autoplay: {
+  //   daly: 6000,
+  // },
+
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
